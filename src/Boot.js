6c18aebc4b4970d26fcfe45d743be8ac04bbba8b
Malav Desai <malav1410@gmail.com>
2015-02-12 17:10:03 +0530

    images and loading bar added in preload function in Preloader state

---
+++ src/Boot.js
@@ -0,0 +1,23 @@
+var Number = {};
+
+
+Number.Boot = function(game){
+  console.log("%cStarting my awesome game", "color:white; background:red");
+};
+
+Number.Boot.prototype = {
+  preload: function() {
+    this.load.image("preloaderBg", "img/loading-bg.png" );
+    this.load.image("preloaderBar", "img/loading-bar.png");
+
+  },
+  create: function() {
+    this.input.maxPointers = 1;
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.scale.setScreenSize(true);
+    this.state.start('Preloader');
+  }
+
+};
---
+++ src/Preloader.js
@@ -0,0 +1,32 @@
+Number.Preloader = function(game) {
+  console.log("%cPreloading my awesome game", "color:white; background:red");
+
+  // Define width and height of the game
+  Number.GAME_WIDTH = 900;
+  Number.GAME_HEIGHT = 600;
+};
+
+Number.Preloader.prototype = {
+  prelod: function () {
+
+    // Set background color and preload image
+    this.stage.backgroundColor = 'C688FF';
+    this.preloadBg = this.add.sprite((900-297)/2, (600-145)/2, 'preloaderBg');
+    this.preloadBar = this.add.sprite((900-158)/2, (600-50)/2, 'preloaderBar');
+    this.load.setPreloadSprite(this.preloadBar);
+
+    // Load images
+    this.load.image('header','img/header.png');
+    this.load.image('button-pause', 'img/button-pause.png');
+    this.image.load('button-start', 'img/button-start.png');
+
+    // If you have any spritesheets then load them here.
+    // this.load.spritesheet('button-audio', 'img/button-audio.png', 35, 35);
+  },
+  
+  create: function () {
+    // start the MainMenu state
+    this.state.start('MainMenu');
+  }
+
+};
