53d5f471314d6d71e950310cc58c2857568c7cea
Malav Desai <malav1410@gmail.com>
2015-02-13 15:52:35 +0530

    All states are done for Number game

--- src/Game.js
+++ src/Game.js
@@ -13,14 +13,15 @@ Number.Game.prototype = {
     pauseButton = this.add.button(850, 8, 'button-pause', this.managePause, this);
 
 
-    txt = this.add.text( 100, 100, 'Click to finish game!', { font: "25px Arial", fill: "#F90600", align: "center" });
+    txt = this.add.text( this.world.centerX - 150, 100, 'Click on a counter to stop!', { font: "25px Arial", fill: "#F90600", align: "center" });
     text = this.add.text(this.world.centerX, this.world.centerY, 'Counter: 0', { font: "64px Arial", fill: "#ffffff", align: "center" });
     text.anchor.setTo(0.5, 0.5);
+    text.inputEnabled = true;
 
     timer = this.time.events.loop(Phaser.Timer.SECOND, this.updateCounter, this);
 
     //  Click to remove
-    this.input.onDown.add(this.removeCounter, this);
+    text.events.onInputDown.add(this.removeCounter, this);
 
   },
   
@@ -41,18 +42,17 @@ Number.Game.prototype = {
   },
 
   managePause: function() {
-    if (this.game.paused === false) {
-      // pause the game
-      this.game.paused = true;
-      // add proper informational text
-      var pausedText = this.add.text(180, 250, "Game paused.", { font: "50px Arial", fill: "#5800aa", align: "center" });  
-    }
-    else {
+    // pause the game
+    this.game.paused = true;
+    // add proper informational text
+    var pausedText = this.add.text(290, 500, "Game paused.\nTap anywhere to continue.", { font: "30px Arial", fill: "#ffffff", align: "center" });
+    // set event listener for the user's click/tap the screen
+    this.input.onDown.add(function(){
       // remove the pause text
       pausedText.destroy();
       // unpause the game
       this.game.paused = false;
-    }
+    }, this);
   },
 
   showCount: function() {
